a4c5cdcbdd14cbc44732c735f0243ca17390e38e
Jhonathan Alfonso <salajhon69@gmail.com>
2020-10-30 13:23:36 -0500

    Project init

---
+++ src/main/java/guru/springframework/spfpetclinit/SpfPetClinitApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spfpetclinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpfPetClinitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpfPetClinitApplication.class, args);
+    }
+
+}
